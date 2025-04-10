900548961ca287888e802c0fbd1f3b6c877c8572
jvazquez-evo <jvazquez@evotecnia.com.ar>
2025-04-10 13:10:12 -0300

    Online/Offline html

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,22 @@
 const socket = io();
+
+const lblOnline = document.getElementById ('on')
+const lblOffline = document.getElementById ('off')
+
+
+socket.on('connect', () =>{
+    console.log ('Conectado')
+
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+
+});
+
+socket.on('disconnect', () =>{
+    console.log('Desconectado del servidor')
+
+    
+    lblOnline.style.display = 'none';
+    lblOffline.style.display = '';
+    
+})
